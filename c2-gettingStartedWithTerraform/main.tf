9c0453998ca4ec81e7494056a68b41f4616d3bb5
iramoz95 <ian.ramoz95@gmail.com>
2022-11-26 09:38:44 -0600

    Deploying a single server

---
+++ c2-gettingStartedWithTerraform/main.tf
@@ -0,0 +1,22 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws",
+#       version = "~> 4.0"
+#     }
+#   }
+#   required_version = ">= 0.14.9"
+# }
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0ab4d1e9cf9a1215a"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "terraform-example"
+  }
+}
